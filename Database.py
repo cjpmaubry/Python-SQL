27a8126f33d7357019eecc24acc708fc5b6c62fd
cjpmaubry <cjpm.aubry@gmail.com>
2020-01-11 14:44:06 +0100

    Added first possibility (display the database)

--- Database.py
+++ Database.py
@@ -1,36 +1,6 @@
 import mysql.connector
 from flask import Flask, render_template
 
-mydb=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="esilvs6"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("show databases")
-
-for i in mycursor:
-    print(i)
-mycursor.close()
-mydb.close()
-
-
-
-cnx = mysql.connector.connect(user='root', password='esilvs6', database='voituredb')
-cursor = cnx.cursor()
-
-query = ('SELECT mark, model, maxspeed, consumption FROM voiture WHERE id=1')
-cursor.execute(query)
-
-for (mark, model, maxspeed, consumption) in cursor:
-    print("The ID number 1 refers to  {} {}, max speed : {} km/h, consumption : {} l/100km".format(mark, model, maxspeed, consumption))
-
-cursor.close()
-cnx.close()
-
-
 
 app = Flask(__name__)
 
@@ -40,7 +10,19 @@ def home():
 
 @app.route('/display')
 def display():
-    return render_template('display.html')
+
+    mydb = mysql.connector.connect(user='root', password='esilvs6', database='voituredb')
+    cursor = mydb.cursor()
+
+    query = ('SELECT id, mark, model, maxspeed, consumption FROM voiture')
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    cursor.close()
+    mydb.close()
+
+    return render_template('display.html', data=data)
+
 
 @app.route('/request')
 def resquest():
